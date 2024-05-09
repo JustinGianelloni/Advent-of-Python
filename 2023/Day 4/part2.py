73bd5d80a2cca239e8ccce1db16be792ff8ead7a
JustinGianelloni <Justin.Gianelloni@gmail.com>
2024-05-09 16:30:52 -0400

    Day 4 Part 2

--- 2023/Day 4/part2.py
+++ 2023/Day 4/part2.py
@@ -7,9 +7,7 @@ def get_score(winners: list[int], picks: list[int]) -> int:
     for pick in picks:
         if pick in winners:
             count += 1
-    if 0 <= count <= 1:
-        return count
-    return 2 ** (count - 1)
+    return count
 
 def parse_picks(picks: str) -> list[int]:
     split: list[str] = picks.strip('\n').split(' ')
@@ -27,17 +25,33 @@ def parse_winners(winners: str) -> list[int]:
             result.append(int(number))
     return result
 
-def parse_line(line: str) -> int:
+def add_cards(cards: list[int], amount: int, current_card: int) -> None:
+    i: int = 1
+    while i <= amount and current_card + i < len(cards):
+        cards[current_card + i] += 1 * cards[current_card]
+        i += 1
+
+def parse_line(line: str, cards: list[int], current_card: int):
     card: list[str] = line.split(' | ')
     winners: list[int] = parse_winners(card[0].split(': ')[1])
     picks: list[int] = parse_picks(card[1])
-    return get_score(winners, picks)
+    score: int = get_score(winners, picks)
+    add_cards(cards, score, current_card)
+
+def sum_cards(cards: list[int]) -> int:
+    total: int = 0
+    for card in cards:
+        total += card
+    return total
 
 def parse_page(file: TextIOWrapper) -> int:
-    number: int = 0
-    for line in file.readlines():
-        number += parse_line(line)
-    return number
+    lines: list[str] = file.readlines()
+    cards: list[int] = [1] * len(lines)
+    current_card: int = 0
+    for line in lines:
+        parse_line(line, cards, current_card)
+        current_card += 1
+    return sum_cards(cards)
 
 def main() -> None:
     file: TextIOWrapper = open(INPUT)
